5729f0e83ca6f2b454a24908bbe5dfbaeef45af5
Michael Yu <m.yu8088@gmail.com>
2017-10-13 04:03:25 -0500

    more tests

--- src/test/TetrisBoardTest.java
+++ src/test/TetrisBoardTest.java
@@ -9,25 +9,45 @@ import assignment.TetrisBoard;
 
 public class TetrisBoardTest {
 	private static TetrisBoard testBoard;
+	public static final String I = "0 0  1 0  2 0  3 0";
+	public static final String J = "0 0  0 1  1 0  2 0";
+	public static final String L = "0 0  1 0  2 0  2 1";
+	public static final String T = "0 0  1 0  2 0  1 1";
+	public static final String SQUARE = "0 0  0 1  1 0  1 1";
+	public static final String DUCKRIGHT = "0 0  1 0  1 1  2 1";
+	public static final String DUCKLEFT = "0 1  1 1  1 0  2 0";
 	
 	@BeforeClass
 	public static void initBoard() {
-		testBoard = new TetrisBoard(20,10);
+		// Base board used for testing
+		testBoard = new TetrisBoard(10,20);
 	}
 	
 	@Test
 	public void validTest() {
+		// Out of bounds locations should fail test
 		assertFalse(testBoard.valid(-10, 20));
 		assertFalse(testBoard.valid(20, 10));
+		assertFalse(testBoard.valid(5, -5));
 		
+		// In bounds locations should pass test
 		assertTrue(testBoard.valid(0, 0));
-		assertFalse(testBoard.valid(19, 9));
+		assertTrue(testBoard.valid(9, 19));
+		assertTrue(testBoard.valid(5, 5));
 	}
 	
 	@Test
 	public void equalsTest() {
-		TetrisBoard otherBoard = new TetrisBoard(20,10);
-		assertTrue("failed assertTrue", testBoard.equals(otherBoard));
+		// Board should equal itself
+		assertTrue(testBoard.equals(testBoard));
+		
+		// Same size board with no actions applied should pass
+		TetrisBoard otherBoard = new TetrisBoard(10,20);
+		assertTrue(testBoard.equals(otherBoard));
+		
+		// Different size board with no actions applied should fail
+		otherBoard = new TetrisBoard(15,10);
+		assertFalse(testBoard.equals(otherBoard));
 	}
 
 }
--- src/test/TetrisPieceTest.java
+++ src/test/TetrisPieceTest.java
@@ -12,51 +12,102 @@ import assignment.Piece;
 import assignment.TetrisPiece;
 
 public class TetrisPieceTest {
-	private static Piece J;
-	private static Piece L;
-	private static Piece T;
-	private static Piece SQUARE;
-	private static Piece STICK;
-	private static Piece LEFTDOG;
-	private static Piece RIGHTDOG;
-	
-	@BeforeClass
-	public static void initPieces() {
-		J = TetrisPiece.getPiece("0 0  1 0  1 1  1 2");
-		L = TetrisPiece.getPiece("0 0  0 1  0 2  1 0");
-		T = TetrisPiece.getPiece("0 0  1 0  2 0  1 1");
-		SQUARE = TetrisPiece.getPiece("0 0  1 0  0 1  1 1");
-		STICK = TetrisPiece.getPiece("0 0  1 0  2 0  3 0");
-		LEFTDOG = TetrisPiece.getPiece("1 0  2 0  0 1  1 1");
-		RIGHTDOG = TetrisPiece.getPiece("0 0  1 0  1 1  2 1");
-	}
+	public static final TetrisPiece I = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  2 0  3 0");
+	public static final TetrisPiece J = (TetrisPiece) TetrisPiece.getPiece("0 0  0 1  1 0  2 0");
+	public static final TetrisPiece L = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  2 0  2 1");
+	public static final TetrisPiece T = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  2 0  1 1");
+	public static final TetrisPiece SQUARE = (TetrisPiece) TetrisPiece.getPiece("0 0  0 1  1 0  1 1");
+	public static final TetrisPiece DUCKRIGHT = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  1 1  2 1");
+	public static final TetrisPiece DUCKLEFT = (TetrisPiece) TetrisPiece.getPiece("0 1  1 1  1 0  2 0");
 	
 	@Test
 	public void equalsTest() {
-		System.out.println("0 0  1 0  1 1  1 2 should equal J");
-		Piece testPiece = TetrisPiece.getPiece("0 0  1 0  1 1  1 2"); // same rotation as J
-		boolean testEquals = testPiece.equals(TetrisPieceTest.J);
-		assertTrue(testEquals);
+		TetrisPiece testPiece = (TetrisPiece) TetrisPiece.getPiece("0 0  0 1  1 0  2 0"); // same rotation as J
+		// Should equal itself
+		assertEquals(testPiece, testPiece);
+		// "0 0  0 1  1 0  2 0" (J) should equal J
+		assertEquals(testPiece, J);
+		// "0 0  0 1  1 0  2 0" (J) should NOT equal L
+		assertNotEquals(testPiece, L);
+		// "0 0  0 1  1 0  2 0" (J) should NOT equal T
+		assertNotEquals(testPiece, T);
 		
-		System.out.println("0 0  0 1  0 2  1 0 should NOT equal L");
-		testEquals = testPiece.equals(TetrisPieceTest.L);
-		assertFalse(testEquals);
+		// "0 0  1 0  1 1  2 1" (DUCKRIGHT) should equal DUCKRIGHT
+		testPiece = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  1 1  2 1"); // same rotation as DUCKRIGHT
+		assertEquals(testPiece, DUCKRIGHT);
+		// "0 0  0 1  1 0  2 0" (DUCKLEFT) should NOT equal DUCKLEFT
+		assertNotEquals(testPiece, DUCKLEFT);
+		// "0 0  0 1  1 0  2 0" (J) should NOT equal T
+		assertNotEquals(testPiece, T);
 		
-		System.out.println();
+		// T should not equal anything except itself
+		testPiece = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  2 0  1 1"); // same rotation as T
+		assertEquals(testPiece, T);
+		assertNotEquals(testPiece, L);
+		assertNotEquals(testPiece, J);
+		assertNotEquals(testPiece, SQUARE);
+		assertNotEquals(testPiece, I);
+		assertNotEquals(testPiece, DUCKRIGHT);
+		assertNotEquals(testPiece, DUCKLEFT);
+		// Should equal itself
+		assertEquals(testPiece, testPiece);
+		
+		// Random group of points should not equal any of the pieces
+		testPiece = (TetrisPiece) TetrisPiece.getPiece("0 1  1 1  2 1  1 3");
+		assertNotEquals(testPiece, L);
+		assertNotEquals(testPiece, J);
+		assertNotEquals(testPiece, SQUARE);
+		assertNotEquals(testPiece, I);
+		assertNotEquals(testPiece, DUCKRIGHT);
+		assertNotEquals(testPiece, DUCKLEFT);
+		assertNotEquals(testPiece, T);
 	}
 	
 	@Test
 	public void bodyEqualsTest() {
-		System.out.println("0 0  1 0  1 1  1 2 should have same body as J");
-		Piece testPiece = TetrisPiece.getPiece("0 0  1 0  1 1  1 2"); // same rotation as J
+		Piece testPiece = TetrisPiece.getPiece("0 0  0 1  1 0  2 0"); // same rotation as J
 		Point[] body = testPiece.getBody();
+		
+		// J should have same body as J in same rotation
 		boolean testEquals = ((TetrisPiece) TetrisPieceTest.J).bodyEquals(new HashSet<Point>(Arrays.asList(body)));
 		assertTrue(testEquals);
-		
-		System.out.println("0 0  1 0  1 1  1 2 should NOT have same body as L");
+		// J should not have same body as L in same rotation
 		testEquals = ((TetrisPiece) TetrisPieceTest.L).bodyEquals(new HashSet<Point>(Arrays.asList(body)));
 		assertFalse(testEquals);
 		
-		System.out.println();
+		// DUCKRIGHT should have same body as DUCKRIGHT in same rotation
+		testPiece = (TetrisPiece) TetrisPiece.getPiece("0 0  1 0  1 1  2 1"); // same rotation as DUCKRIGHT
+		body = testPiece.getBody();
+		testEquals = ((TetrisPiece) TetrisPieceTest.DUCKRIGHT).bodyEquals(new HashSet<Point>(Arrays.asList(body)));
+		assertTrue(testEquals);
+		// DUCKLEFT should not have same body as DUCKRIGHT in same rotation
+		testEquals = ((TetrisPiece) TetrisPieceTest.DUCKLEFT).bodyEquals(new HashSet<Point>(Arrays.asList(body)));
+		assertFalse(testEquals);
+	}
+	
+	@Test
+	public void rotateTest() { // No wall kicks, just static testing of piece rotations
+		// First rotation of J should equal hardcoded string
+		Piece testPiece = TetrisPiece.getPiece("0 0  1 0  1 1  1 2"); // 1st rotation of J counterclockwise
+		assertEquals(J.nextRotation(), testPiece);
+		// First rotation of J rotated three more times should equal J 
+		testPiece = testPiece.nextRotation().nextRotation().nextRotation();
+		assertEquals(J, testPiece);
+		
+		// First rotation of I should equal hardcoded string
+		testPiece = TetrisPiece.getPiece("0 0  0 1  0 2  0 3"); // 1st rotation of I counterclockwise
+		assertEquals(I.nextRotation(), testPiece);
+		// First rotation of J rotated three more times should equal J 
+		testPiece = testPiece.nextRotation().nextRotation().nextRotation();
+		assertEquals(I, testPiece);
+		
+		// Pieces should not be equal to any of its rotations (except in case of SQUARE)
+		testPiece = TetrisPiece.getPiece("0 0  1 0  2 0  1 1"); // Initial state of T
+		assertEquals(T, testPiece);
+		assertNotEquals(T, testPiece.nextRotation());
+		assertNotEquals(T, testPiece.nextRotation().nextRotation());
+		assertNotEquals(T, testPiece.nextRotation().nextRotation().nextRotation());
+		// Should equal itself after rotating 4 times
+		assertEquals(T, testPiece.nextRotation().nextRotation().nextRotation().nextRotation());
 	}
 }
